830be69b7c1660ed0ff5e27c209cb18e35b99040
Mikko Leppänen <mleppan23@gmail.com>
2021-09-11 09:45:28 +0300

    Part3, exercise 3.9: Backend synced with the frontend.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(
     ":method :url :status :res[content-length] - :response-time ms - :body"
   )
 );
+app.use(cors());
 
 let persons = [
   {
@@ -98,7 +100,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
